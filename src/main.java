113255bee2795eb4cb0418a792fa10d60681738b
itsmihirrao <39073078+itsmihirrao@users.noreply.github.com>
2020-06-19 15:00:49 -0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,9 +1,11 @@
 public class main {
 	public static void main(String[] args){
-		String[][] table = {{"Fruits", "Sodas", "Cars"},
-							{"apples", "Fanta", "Ferrari"},
-							{"oranges", "Coke", "Lamborghini"},
-							{"mangoes", "Mountain Dew", "Porsche"}};
+		String[][] table = {{"Date", "Humidity", "Pressure", "Rain"},
+                            {"January 1", "93%", "999.7", "Rain"},
+                            {"January 2", "49%", "1015.5", "No Rain"},
+                            {"January 3", "79%", "1031.1", "No Rain"},
+                            {"January 4", "65%", "984.9", "Rain"},
+                            {"January 5", "90%", "975.2", "Rain"}};
 
 		tableGen a = new tableGen(table);
 		a.createTable();
